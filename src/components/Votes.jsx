2c28f130c40302aa23cb1447c1ae16f6d57407c3
16000psi <davidjhnsmith@gmail.com>
2023-03-14 15:40:20 +0000

    add votes error message

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -4,6 +4,7 @@ import { incrementVotes } from "../api"
 const Votes = ({id, votes}) => {
 
     const [votesNumber, setVotesNumber] = useState(votes)
+    const [voteError, setVoteError] = useState(false)
 
     function activateUpVote (event) {
 
@@ -15,6 +16,8 @@ const Votes = ({id, votes}) => {
 
 
         incrementVotes(id, 1).catch(() => {
+
+            setVoteError(true)
 
             setVotesNumber((currentVotesNumber) => {
                 const newVotes = currentVotesNumber - 1
@@ -36,6 +39,8 @@ const Votes = ({id, votes}) => {
 
         incrementVotes(id, -1).catch(() => {
 
+            setVoteError(true)
+
             setVotesNumber((currentVotesNumber) => {
                 const newVotes = currentVotesNumber + 1
                 return newVotes
@@ -46,12 +51,22 @@ const Votes = ({id, votes}) => {
     }
 
 
-    return (
+    return (<>
+        {!voteError &&
         <section className='review-votes'>
             <p>{votesNumber} votes</p>
             <button className="vote-button" onClick={activateUpVote}><p className="vote-innertext">+</p> </button>
             <button className="vote-button" onClick={activateDownVote}><p className="vote-innertext">-</p></button>
         </section>
+        }
+        {voteError &&
+                <section className='review-votes'>
+                <p>Error - plase try again later</p>
+
+            </section>
+        
+        }
+        </>
     )
 }
 
