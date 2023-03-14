42651cbfbf8d55c800edb45f49df6474d823b5ec
16000psi <davidjhnsmith@gmail.com>
2023-03-14 14:25:47 +0000

    6 add view comments by review

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,53 +1,33 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import {PostComment, Comment} from "./index"
+import { getCommentsByReview } from "../api"
+
+
 
 const Comments = ({review_id}) => {
 
-    const [ commentsData, setCommentsData] = useState([
-        {
-          comment_id: 19,
-          body: "Quis duis mollit ad enim deserunt.",
-          review_id: 3,
-          author: "jessjelly",
-          votes: 3,
-          created_at: "2021-03-27T19:48:58.110Z"
-        },
-        {
-          comment_id: 20,
-          body: "Laboris nostrud ea ex occaecat aute quis consectetur anim.",
-          review_id: 3,
-          author: "cooljmessy",
-          votes: 17,
-          created_at: "2021-03-27T14:15:38.110Z"
-        },
-        {
-          comment_id: 21,
-          body: "Consequat nisi dolor nulla esse sunt eu ipsum laborum deserunt duis.",
-          review_id: 3,
-          author: "weegembump",
-          votes: 1,
-          created_at: "2021-03-27T14:15:36.110Z"
-        },
-        {
-          comment_id: 22,
-          body: "Ex id ipsum dolore non cillum anim sint duis nisi anim deserunt nisi minim.",
-          review_id: 3,
-          author: "jessjelly",
-          votes: 9,
-          created_at: "2021-03-27T14:15:31.110Z"
-        },
-        {
-          comment_id: 23,
-          body: "Commodo aliquip sunt commodo elit in esse velit laborum cupidatat anim.",
-          review_id: 3,
-          author: "happyamy2016",
-          votes: 10,
-          created_at: "2021-03-27T14:15:21.110Z"
-        }
-    ])
+    const [ commentsData, setCommentsData] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
 
 
-    return (
+    useEffect(() => {
+      setIsLoading(true)
+      getCommentsByReview(review_id).then(({data}) => {
+        const {comments} = data
+        setIsLoading(false)
+        setCommentsData(comments);
+      }).catch((error) =>
+      console.log(error));
+    }, [review_id]);
+
+    
+
+
+
+
+    return (<>
+
+        {!isLoading &&
         <section className='comments-container'>
             <PostComment review_id={review_id} />
 
@@ -58,10 +38,14 @@ const Comments = ({review_id}) => {
                     return <Comment key={comment.comment_id} commentObject={comment} />
                 })
                 :
-                <h1>There are no comments for this post yet.</h1>
+                <h2>There are no comments for this post yet.</h2>
             }
         </section>
-    )
+        }
+        {isLoading &&
+        <h2>Loading</h2>
+        }
+        </>)
 }
 
 export default Comments;
