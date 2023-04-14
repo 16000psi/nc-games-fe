4d588f804972f1e6ea148f23ca559f205c1d4743
16000psi <davidjhnsmith@gmail.com>
2023-04-14 12:18:50 +0100

    improve layout and loading

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -1,12 +1,24 @@
 import { useState } from "react"
 import { incrementVotes } from "../api"
+import { useContext } from 'react';
+import { UserContext } from '../context/UserContext';
 
-const Votes = ({id, votes, setVotesIncrement}) => {
+const Votes = ({ id, votes, setVotesIncrement }) => {
 
     const [votesNumber, setVotesNumber] = useState(votes)
     const [voteError, setVoteError] = useState(false)
+    const { user } = useContext(UserContext);
 
-    function activateUpVote (event) {
+    function activateUpVote(event) {
+
+        event.stopPropagation()
+
+        let upvoted = JSON.parse(localStorage.getItem(`${user.username}-${id}-upvotes`))
+        let downvoted = JSON.parse(localStorage.getItem(`${user.username}-${id}-downvotes`))
+
+
+
+
 
         setVotesIncrement((currentVotesIncrement) => {
             const newVotesIncrement = currentVotesIncrement + 1
@@ -17,8 +29,6 @@ const Votes = ({id, votes, setVotesIncrement}) => {
             const newVotes = currentVotesNumber + 1
             return newVotes
         })
-        event.stopPropagation()
-
 
         incrementVotes(id, 1).catch(() => {
 
@@ -31,22 +41,35 @@ const Votes = ({id, votes, setVotesIncrement}) => {
 
         }
         )
+        console.log(downvoted, "<<<< downvoted")
+        console.log(upvoted, "<<<< upvoted")
+
+
     }
 
-    function activateDownVote (event) {
+
+
+
+
+
+    function activateDownVote(event) {
+        event.stopPropagation()
+
+
+        let upvoted = JSON.parse(localStorage.getItem(`${user.username}-${id}-upvotes`))
+        let downvoted = JSON.parse(localStorage.getItem(`${user.username}-${id}-downvotes`))
+
+
+
 
         setVotesIncrement((currentVotesIncrement) => {
             const newVotesIncrement = currentVotesIncrement - 1
             return newVotesIncrement
         })
-
         setVotesNumber((currentVotesNumber) => {
             const newVotes = currentVotesNumber - 1
             return newVotes
         })
-        event.stopPropagation()
-
-
         incrementVotes(id, -1).catch(() => {
 
             setVoteError(true)
@@ -58,25 +81,31 @@ const Votes = ({id, votes, setVotesIncrement}) => {
 
         }
         )
+
+
+        console.log(downvoted, "<<<< downvoted")
+        console.log(upvoted, "<<<< upvoted")
+
+
     }
 
 
     return (<>
         {!voteError &&
-        <section className='review-votes'>
-            <p>{votesNumber} votes</p>
-            <button className="vote-button" onClick={activateUpVote}><p className="vote-innertext">+</p> </button>
-            <button className="vote-button" onClick={activateDownVote}><p className="vote-innertext">-</p></button>
-        </section>
+            <section className='review-votes'>
+                <p>{votesNumber} votes</p>
+                <button className="vote-button" onClick={activateUpVote}><p className="vote-innertext">+</p> </button>
+                <button className="vote-button" onClick={activateDownVote}><p className="vote-innertext">-</p></button>
+            </section>
         }
         {voteError &&
-                <section className='review-votes'>
+            <section className='review-votes'>
                 <p>Error - plase try again later</p>
 
             </section>
-        
+
         }
-        </>
+    </>
     )
 }
 
