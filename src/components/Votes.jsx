6b403d7eaa93cebc58d2b0fd641318c59d532ae9
16000psi <davidjhnsmith@gmail.com>
2023-04-16 11:36:57 +0100

    fix votes crash when logged out

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -12,98 +12,107 @@ const Votes = ({ id, votes, setVotesIncrement }) => {
     const [isDownVoted, setIsDownVoted] = useState(false)
 
     useEffect(() => {
-        let upvoted = JSON.parse(localStorage.getItem(`${user.username}-${id}-upvotes`))
-        let downvoted = JSON.parse(localStorage.getItem(`${user.username}-${id}-downvotes`))
-        if (upvoted && upvoted === 1) {
-            setIsUpVoted(true)
-        } else if (downvoted && downvoted === 1) {
-            setIsDownVoted(true)
+
+        if (user) {
+            let upvoted = JSON.parse(localStorage.getItem(`${user.username}-${id}-upvotes`))
+            let downvoted = JSON.parse(localStorage.getItem(`${user.username}-${id}-downvotes`))
+            if (upvoted && upvoted === 1) {
+                setIsUpVoted(true)
+            } else if (downvoted && downvoted === 1) {
+                setIsDownVoted(true)
+            }
         }
-    }, [id, user.username])
+    }, [id, user])
 
     function activateUpVote(event) {
 
         event.stopPropagation()
 
-        let upvoted = JSON.parse(localStorage.getItem(`${user.username}-${id}-upvotes`))
-        let downvoted = JSON.parse(localStorage.getItem(`${user.username}-${id}-downvotes`))
+        if (user) {
 
-        if ((upvoted && upvoted === 0) || !upvoted) {
+            let upvoted = JSON.parse(localStorage.getItem(`${user.username}-${id}-upvotes`))
+            let downvoted = JSON.parse(localStorage.getItem(`${user.username}-${id}-downvotes`))
 
-            setVotesIncrement((currentVotesIncrement) => {
-                const newVotesIncrement = currentVotesIncrement + 1
-                return newVotesIncrement
-            })
+            if ((upvoted && upvoted === 0) || !upvoted) {
 
-            setVotesNumber((currentVotesNumber) => {
-                const newVotes = currentVotesNumber + 1
-                return newVotes
-            })
-
-            incrementVotes(id, 1).catch(() => {
-
-                setVoteError(true)
-
-                setVotesNumber((currentVotesNumber) => {
-                    const newVotes = currentVotesNumber - 1
-                    return newVotes
+                setVotesIncrement((currentVotesIncrement) => {
+                    const newVotesIncrement = currentVotesIncrement + 1
+                    return newVotesIncrement
                 })
-
-            }
-            )
-
-            if (downvoted && downvoted === 1) {
-                localStorage.setItem(`${user.username}-${id}-downvotes`, JSON.stringify(0))
-                setIsDownVoted(false)
-            } else {
-                localStorage.setItem(`${user.username}-${id}-upvotes`, JSON.stringify(1))
-                setIsUpVoted(true)
-            }
-
-
-        }
-    }
-
-    function activateDownVote(event) {
-        event.stopPropagation()
-
-
-        let upvoted = JSON.parse(localStorage.getItem(`${user.username}-${id}-upvotes`))
-        let downvoted = JSON.parse(localStorage.getItem(`${user.username}-${id}-downvotes`))
-
-
-
-        if ((downvoted && downvoted === 0) || !downvoted) {
-
-
-
-            setVotesIncrement((currentVotesIncrement) => {
-                const newVotesIncrement = currentVotesIncrement - 1
-                return newVotesIncrement
-            })
-            setVotesNumber((currentVotesNumber) => {
-                const newVotes = currentVotesNumber - 1
-                return newVotes
-            })
-            incrementVotes(id, -1).catch(() => {
-
-                setVoteError(true)
 
                 setVotesNumber((currentVotesNumber) => {
                     const newVotes = currentVotesNumber + 1
                     return newVotes
                 })
 
-            }
-            )
-            if (upvoted && upvoted === 1) {
-                localStorage.setItem(`${user.username}-${id}-upvotes`, JSON.stringify(0))
-                setIsUpVoted(false)
-            } else {
-                localStorage.setItem(`${user.username}-${id}-downvotes`, JSON.stringify(1))
-                setIsDownVoted(true)
+                incrementVotes(id, 1).catch(() => {
+
+                    setVoteError(true)
+
+                    setVotesNumber((currentVotesNumber) => {
+                        const newVotes = currentVotesNumber - 1
+                        return newVotes
+                    })
+
+                }
+                )
+
+                if (downvoted && downvoted === 1) {
+                    localStorage.setItem(`${user.username}-${id}-downvotes`, JSON.stringify(0))
+                    setIsDownVoted(false)
+                } else {
+                    localStorage.setItem(`${user.username}-${id}-upvotes`, JSON.stringify(1))
+                    setIsUpVoted(true)
+                }
+
+
             }
         }
+    }
+
+    function activateDownVote(event) {
+        event.stopPropagation()
+
+        if (user) {
+
+            let upvoted = JSON.parse(localStorage.getItem(`${user.username}-${id}-upvotes`))
+            let downvoted = JSON.parse(localStorage.getItem(`${user.username}-${id}-downvotes`))
+
+            if ((downvoted && downvoted === 0) || !downvoted) {
+
+
+
+                setVotesIncrement((currentVotesIncrement) => {
+                    const newVotesIncrement = currentVotesIncrement - 1
+                    return newVotesIncrement
+                })
+                setVotesNumber((currentVotesNumber) => {
+                    const newVotes = currentVotesNumber - 1
+                    return newVotes
+                })
+                incrementVotes(id, -1).catch(() => {
+
+                    setVoteError(true)
+
+                    setVotesNumber((currentVotesNumber) => {
+                        const newVotes = currentVotesNumber + 1
+                        return newVotes
+                    })
+
+                }
+                )
+                if (upvoted && upvoted === 1) {
+                    localStorage.setItem(`${user.username}-${id}-upvotes`, JSON.stringify(0))
+                    setIsUpVoted(false)
+                } else {
+                    localStorage.setItem(`${user.username}-${id}-downvotes`, JSON.stringify(1))
+                    setIsDownVoted(true)
+                }
+            }
+
+        }
+
+
 
     }
 
@@ -111,8 +120,12 @@ const Votes = ({ id, votes, setVotesIncrement }) => {
         {!voteError &&
             <section className='review-votes'>
                 <p>{votesNumber} votes</p>
-                <button className="vote-button" style={(isUpVoted) ? {color: "green"} : {color: "white"}} onClick={activateUpVote}><p className="vote-innertext">+</p> </button>
-                <button className="vote-button" style={(isDownVoted) ? {color: "red"} : {color: "white"}} onClick={activateDownVote}><p className="vote-innertext">-</p></button>
+                {user &&
+                    <>
+                        <button className="vote-button" style={(isUpVoted) ? { color: "green" } : { color: "white" }} onClick={activateUpVote}><p className="vote-innertext">+</p> </button>
+                        <button className="vote-button" style={(isDownVoted) ? { color: "red" } : { color: "white" }} onClick={activateDownVote}><p className="vote-innertext">-</p></button>
+                    </>
+                }
             </section>
         }
         {voteError &&
